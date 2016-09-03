b85fb6ec36e94871c477b8ea2aab62ea4f076447
banta <banta.bernard@gmail.com>
2016-09-03 22:18:28 +0300

    Added local storage module

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,36 +1,36 @@
-var app = angular.module("app", ['ngRoute', 'ui.router', 'ngMaterial', 'ngMessages']);
+var app = angular.module("app", ['ngRoute', 'ui.router', 'ngMaterial', 'ngMessages', 'app.utils']);
 app.config(function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: '/templates/home.html',
-        controller: 'HomeCtrl'
-      })
-      
-      .state('about', {
-        url: '/about',
-        templateUrl: '/templates/about.html',
-        controller: 'AboutCtrl'
-      })
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/templates/home.html',
+            controller: 'HomeCtrl'
+        })
 
-      .state('sign_in', {
-          url: '/sign_in',
-          templateUrl: '/templates/sign_in.html',
-          controller: 'SignInCtrl'
-      })
+        .state('about', {
+            url: '/about',
+            templateUrl: '/templates/about.html',
+            controller: 'AboutCtrl'
+        })
 
-      .state('sign_up', {
-          url: '/sign_up',
-          templateUrl: '/templates/sign_up.html',
-          controller: 'SignUpCtrl'
-      })
+        .state('sign_in', {
+            url: '/sign_in',
+            templateUrl: '/templates/sign_in.html',
+            controller: 'SignInCtrl'
+        })
 
-      .state('forgot_password', {
-          url: '/forgot_password',
-          templateUrl: '/templates/forgot_password.html',
-          controller: 'ForgotPasswordCtrl'
-      })
+        .state('sign_up', {
+            url: '/sign_up',
+            templateUrl: '/templates/sign_up.html',
+            controller: 'SignUpCtrl'
+        })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('home');
+        .state('forgot_password', {
+            url: '/forgot_password',
+            templateUrl: '/templates/forgot_password.html',
+            controller: 'ForgotPasswordCtrl'
+        })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('home');
 });
