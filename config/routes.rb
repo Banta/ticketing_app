ec56f2a2d85d9aeaeecf68047566fb9d376c35e9
banta <banta.bernard@gmail.com>
2016-09-03 22:51:00 +0300

    Added api user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: {format: :json}, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show, :create, :update, :destroy]
+    end
+  end
+
   root 'static_pages#index'
 end
