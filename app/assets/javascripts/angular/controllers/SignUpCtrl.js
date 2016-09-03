6da9f5a8869a27a412d132afd019fa1863cc9142
banta <banta.bernard@gmail.com>
2016-09-04 00:18:29 +0300

    If registration info is valid, send the info to the user service

--- app/assets/javascripts/angular/controllers/SignUpCtrl.js
+++ app/assets/javascripts/angular/controllers/SignUpCtrl.js
@@ -1,4 +1,4 @@
-app.controller('SignUpCtrl', function ($scope) {
+app.controller('SignUpCtrl', function ($scope, $localstorage, User) {
     $scope.showErrors = false
     $scope.data = {}
 
@@ -7,6 +7,25 @@ app.controller('SignUpCtrl', function ($scope) {
         // Check if form has errors
         if (userForm.$invalid == true) {
             $scope.showErrors = true
+        } else {
+            var user = new User({user: $scope.data}); // *Note: setting the bound param (`id` in this case) is *required*
+            user.$save();
+            user.$promise.then(
+                function (data) {
+                    $localstorage.setObject('user', data);
+                    console.log('User was successfully created: ' + JSON.stringify(data));
+                    $scope.redirect('sign_in')
+                },
+                function (err) {
+                    console.log('Error saving user.');
+                    var message = ""
+                    $.each(err.data.errors, function (key, value) {
+                        message = message + key + ": " + value + "\n"
+                    });
+                    
+                    console.log(message)
+                }).finally(function () {
+            });
         }
     }
 });
