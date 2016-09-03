9f9d2b0a3e4a7857610a48eae2cc7570379093f3
banta <banta.bernard@gmail.com>
2016-09-03 22:37:52 +0300

    Created user and base controller for devise

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+  respond_to :json
+
+  def show
+    respond_with User.find(params[:id])
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      user.confirm
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = current_user
+
+    if user.update_attributes(user_params)
+      user.confirm
+      render json: user, status: 200, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    head 204
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
