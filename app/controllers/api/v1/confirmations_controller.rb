67ed84503bcec16af97b059d670bffc31d882043
banta <banta.bernard@gmail.com>
2016-09-04 13:36:58 +0300

    Added controller for email confirmation

---
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::ConfirmationsController < Api::V1::BaseController
+  respond_to :json
+
+  def create
+    confirmation_token = params[:confirmation_token]
+    user = User.find_by(auth_token: confirmation_token)
+
+    if user
+      user.generate_authentication_token!
+      user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: 'Ivalid token' }, status: 404
+    end
+  end
+end
