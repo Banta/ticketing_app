d1e1351770766981b1f1bd5813c2f46415c0446c
banta <banta.bernard@gmail.com>
2016-09-04 23:34:14 +0300

    Implemented forgot password controller

--- app/assets/javascripts/angular/controllers/ForgotPasswordCtrl.js
+++ app/assets/javascripts/angular/controllers/ForgotPasswordCtrl.js
@@ -1,3 +1,33 @@
-app.controller('ForgotPasswordCtrl', function($scope) {
-    $scope.message = "This page will be used to display add student form";
+app.controller('ForgotPasswordCtrl', function($scope, UserPassword) {
+
+    // Redirect user to home page if they have already signed in
+    if ($scope.signed_in) {
+        $scope.flashAlert('You are already signed in.')
+        $scope.redirect('home')
+    }
+
+    $scope.showErrors = false;
+    $scope.data = {};
+
+    $scope.submitForm = function (userForm, data) {
+        if (userForm.$invalid == true) {
+            $scope.showErrors = true
+        } else {
+            $scope.showProgress('Please wait...')
+            var user = new UserPassword({user: $scope.data});
+            user.$save()
+            user.$promise.then(
+                function (data) {
+                    $localstorage.setObject('user', data);
+                    console.log('Request sent successfully: ' + JSON.stringify(data))
+                    $scope.redirect('home')
+                },
+                function (err) {
+                    console.log('Error saving user.');
+                    $scope.flashAlert('Error occurred please contact support.')
+                }).finally(function () {
+                $scope.hideProgress()
+            });
+        }
+    }
 });
