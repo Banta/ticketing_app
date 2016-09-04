77280a41da79d0a1372a6f841e9494512e801cad
banta <banta.bernard@gmail.com>
2016-09-04 13:37:46 +0300

    Added rails route for confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :confirmations, only: [:create]
     end
   end
 
