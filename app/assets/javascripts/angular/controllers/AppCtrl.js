5ae8279e45a538dddae54dd7013e5b39911929e8
banta <banta.bernard@gmail.com>
2016-09-04 17:13:16 +0300

    Hide navigation links depending on whether the user is logged in or not

--- app/assets/javascripts/angular/controllers/AppCtrl.js
+++ app/assets/javascripts/angular/controllers/AppCtrl.js
@@ -1,3 +1,8 @@
-app.controller('AppCtrl', function($scope, $location, $localstorage) {
+app.controller('AppCtrl', function ($scope, $location, $localstorage) {
     console.log('AppCtrl loaded')
+
+    $scope.signed_in = false
+    if ($localstorage.get('auth_token')) {
+        $scope.signed_in = true
+    }
 });
