ca6ba0279f0e055d01a456061a1bcf28e1a329da
banta <banta.bernard@gmail.com>
2016-09-05 01:00:24 +0300

    Generate reset password token in password controller

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -5,10 +5,14 @@ class Api::V1::PasswordsController < Api::V1::BaseController
     user = user_email.present? && User.find_by(email: user_email)
 
     if user
+      # Generate a new authentication tocken that will be used as an email
+      # reseting password
+      user.generate_authentication_token!
+      user.save
                                                                       # Todo: This is not the best way to do i
                                                                       # It should be improved
       UserMailer.send_user_reset_password_email(user.id).deliver! unless Rails.env == 'test'
-      render json: user, status: 200, location: [:api, user]
+      render json: {reset_token: user.auth_token}, status: 200, location: [:api, user]
     else
       render json: { errors: 'Invalid email' }, status: 404
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,12 +13,6 @@ class UserMailer < ApplicationMailer
 
   def send_user_reset_password_email(user_id)
     @user = User.find_by(id: user_id)
-
-    # Generate a new authentication tocken that will be used as an email
-    # reseting password
-    @user.generate_authentication_token!
-    @user.save
-
     mail to: @user.email, subject: 'Reset password email'
   end
 end
