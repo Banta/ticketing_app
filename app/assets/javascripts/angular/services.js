e30f90982452a15033504fadaf3b447013027daa
banta <banta.bernard@gmail.com>
2016-09-04 18:25:19 +0300

    Added UserSession factory

--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -2,9 +2,15 @@ angular.module('app.services', ['ngCachedResource', 'app.utils'])
     .factory('User', function ($cachedResource) {
         return $cachedResource('users', '/users/:id', {id: "@id"})
     })
+
     .factory('Confirmation', function ($cachedResource) {
         return $cachedResource('confirmation', '/confirmations', {id: "@id"})
     })
+
+    .factory('UserSession', function ($cachedResource) {
+        return $cachedResource('sessions', '/sessions/:id', {id: "@id"})
+    })
+
     .factory('UserSession', function ($cachedResource) {
         return $cachedResource('sessions', '/sessions/:id', {id: "@id"})
     })
