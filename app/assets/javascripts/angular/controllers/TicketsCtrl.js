de32395ecda8ca23053a1811e6da0ababf138b57
banta <banta.bernard@gmail.com>
2016-09-05 11:03:53 +0300

    Alert user to sign in if they are not signed in

--- app/assets/javascripts/angular/controllers/TicketsCtrl.js
+++ app/assets/javascripts/angular/controllers/TicketsCtrl.js
@@ -1,3 +1,7 @@
 app.controller('TicketsCtrl', function($scope) {
     $scope.authenticate_user()
 });
+    if ($scope.signed_in === false) {
+        $scope.flashAlert('Please sign in to proceed.')
+        $scope.redirect('sign_in')
+    }
