521ec73cd96a9cabdb831c1915bd843ed1afb7a7
banta <banta.bernard@gmail.com>
2016-09-05 11:05:20 +0300

    Get users tickets

--- app/assets/javascripts/angular/controllers/TicketsCtrl.js
+++ app/assets/javascripts/angular/controllers/TicketsCtrl.js
@@ -1,7 +1,20 @@
-app.controller('TicketsCtrl', function($scope) {
-    $scope.authenticate_user()
-});
+app.controller('TicketsCtrl', function ($scope, Ticket) {
     if ($scope.signed_in === false) {
         $scope.flashAlert('Please sign in to proceed.')
         $scope.redirect('sign_in')
     }
+
+    $scope.showProgress()
+    Ticket.query().$promise.then(
+        function (data) {
+            console.log('Show all tickets ' + JSON.stringify(data))
+            $scope.tickets = data
+        },
+        function (err) {
+            console.log('Show all tickets failed')
+            $scope.flashAlert('Error loading page. Please contact support.')
+        })
+        .finally(function () {
+            $scope.hideProgress()
+        })
+})
