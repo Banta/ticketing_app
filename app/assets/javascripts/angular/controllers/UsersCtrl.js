e255a5736d1cdb241af317f89e61783b637588fd
banta <banta.bernard@gmail.com>
2016-09-07 00:00:50 +0300

    Admin adding user functionality is done

--- app/assets/javascripts/angular/controllers/UsersCtrl.js
+++ app/assets/javascripts/angular/controllers/UsersCtrl.js
@@ -1,4 +1,4 @@
-app.controller('UsersCtrl', function ($scope, User) {
+app.controller('UsersCtrl', function ($scope, User, Auser) {
     if ($scope.signed_in === false) {
         $scope.flashAlert('Please sign in to proceed.')
         $scope.redirect('sign_in')
@@ -9,7 +9,7 @@ app.controller('UsersCtrl', function ($scope, User) {
         $scope.searchUser = ''
 
         $scope.showProgress()
-        User.query().$promise.then(
+        Auser.query().$promise.then(
             function (data) {
                 console.log('All users displayed')
                 $scope.users = data
@@ -21,5 +21,47 @@ app.controller('UsersCtrl', function ($scope, User) {
             .finally(function () {
                 $scope.hideProgress()
             })
+    }
+
+    $scope.showErrors = false
+    $scope.data = {}
+
+    $scope.submitForm = function (userForm, data) {
+
+        // Check if form has errors
+        if (userForm.$invalid == true) {
+            $scope.showErrors = true
+        } else {
+            $('#add-user-modal').modal('toggle')
+            $scope.showProgress()
+            var user = new Auser({user: $scope.data})
+            user.$save()
+            user.$promise.then(
+                function (data) {
+                    console.log('Ticket was successfully created')
+
+                    // Append the newly added ticket
+                    $scope.users.push(data)
+
+                    // Set a successfully alert message for the user
+                    $scope.flashNotice('User was successfully created.')
+                },
+                function (err) {
+                    console.log('Error saving ticket.')
+
+                    // Alert user about the error
+                    $scope.flashAlert(err.data.errors)
+                })
+                .finally(function () {
+                    $scope.hideProgress()
+                    $scope.data = {}
+                })
+        }
+    }
+
+    $scope.closeSubmitForm = function () {
+        $scope.data = {}
+        $scope.showErrors = false
+        $('#add-user-modal').modal('toggle')
     }
 })
