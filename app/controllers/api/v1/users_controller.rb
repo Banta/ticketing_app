1e72ef8f741ad04797b4ba34ee339484a41a4244
banta <banta.bernard@gmail.com>
2016-09-06 21:05:57 +0300

    Added api users index in users contoller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,16 @@ class Api::V1::UsersController < Api::V1::BaseController
   before_action :authenticate_with_token!, only: [:update, :destroy]
   respond_to :json
 
+  def index
+    if current_user.admin?
+      users = User.all.order('created_at DESC')
+
+      render json: users, status: 200
+    else
+      render json: {}, status: 401
+    end
+  end
+
   def show
     respond_with User.find(params[:id])
   end
@@ -9,12 +19,12 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create
     user = User.new(user_params)
     if user.save
-                                                               # Todo: This is not the best way to do i
-                                                               # It should be improved
+      # Todo: This is not the best way to do i
+      # It should be improved
       UserMailer.send_user_confimation_email(user.id).deliver! unless Rails.env == 'test'
       render json: user, status: 201, location: [:api, user]
     else
-      render json: { errors: user.errors }, status: 422
+      render json: {errors: user.errors}, status: 422
     end
   end
 
@@ -24,7 +34,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     if user.update_attributes(user_params)
       render json: user, status: 200, location: [:api, user]
     else
-      render json: { errors: user.errors }, status: 422
+      render json: {errors: user.errors}, status: 422
     end
   end
 
